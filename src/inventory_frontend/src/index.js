7d6ae713577c9e944d3033f01d460315c646e2c5
nyomr <nyomanrichardo11@gmail.com>
2023-12-30 19:45:44 +0700

    Initial commit.

---
+++ src/inventory_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { inventory_backend } from "../../declarations/inventory_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await inventory_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
